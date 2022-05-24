c1b1cfe7703f8749243d0d62ab74009f1a8d6e8c
LoisKOUNINEF <lkouninef@gmail.com>
2022-05-24 11:16:19 +0200

    if else if and logical operators

--- functions/main.c
+++ functions/main.c
@@ -16,6 +16,8 @@ int cube(int num) {
   // or: return num * num * num;
 }
 
+int max(int num1, int num2, int num3);
+
 int main()
 {
 
@@ -25,15 +27,29 @@ int main()
   sayHi("user", 40);
   sayHi("me, myself and I", 35);
 
+  printf("%d is bigger.\n", max(3,5,8));
+  printf("%d is bigger.\n", max(17,8,15));
+  printf("%d is bigger.\n", max(12,28,15));
+
   return 0;
 }
+
+int max(int num1, int num2, int num3) {
+  int result;
+  if(num1 >= num2 && num2 >= num3) {
+    result = num1;
+  } else if(num2 >= num1 && num2 >= num3) {
+    result = num2;
+  } else {
+    result = num3;
+  }
+  return result;
+}
+
+// logical operator '||', '&&', '><=', and '!' same as JS
 
 // void means function will not return anything.
 // int means integer, etc..
 
 // move function to top to avoid conflict,
 // or prototype it before main()
-
-// void sayHi(char name[], int age) {
-//   printf("Hello, %s !", name);
-// }
