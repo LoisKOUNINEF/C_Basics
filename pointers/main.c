6af15f1276174dd0cc726dc23bceb6a48f3c916d
LoisKOUNINEF <lkouninef@gmail.com>
2022-05-30 19:45:39 +0200

    pointers variable

---
+++ pointers/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+  int age = 30;
+  char letter = 'A';
+
+  // pointer variable ==> memory address/location of the variable
+  int * pointerAge = &age;
+  char * pointerLetter = &letter;
+
+  printf("age's memory address : %p\n", &age);
+  printf("letter's memory address : %p\n", &letter);
+
+  return 0;
+}
